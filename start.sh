bb8af4e0f3380030e9162ee00b8b6caabb44e0b3
guaguastandup <45114416+guaguastandup@users.noreply.github.com>
2024-02-19 22:12:45 +0800

    update script

--- start.sh
+++ start.sh
@@ -8,43 +8,52 @@
 # leftBound2
 # rightBound
 
+cleanup() {
+    echo "Caught SIGINT signal, terminating the background processes..."
+    # 杀死所有属于当前脚本进程组的后台进程
+    pkill -P $$
+}
+
+trap cleanup SIGINT
+
+
 cd pkg/system && go build
 
 file="5-0-3000-100-fixed"
-./system 5 0 3000 100 1 0 0 0 0 > ../output/$file.log &
+./system 5 0 3000 100 1 0 0 0 0 > ../output/$file.log
 
 file="10-0-3000-100-fixed"
-./system 10 0 3000 100 1 0 0 0 0 > ../output/$file.log &
+./system 10 0 3000 100 1 0 0 0 0 > ../output/$file.log
 
 file="15-0-3000-100-fixed"
-./system 15 0 3000 100 1 0 0 0 0 > ../output/$file.log &
+./system 15 0 3000 100 1 0 0 0 0 > ../output/$file.log
 
 file="30-0-3000-100-fixed"
-./system 30 0 3000 100 1 0 0 0 0 > ../output/$file.log &
+./system 30 0 3000 100 1 0 0 0 0 > ../output/$file.log
 
 file="60-0-3000-100-fixed"
-./system 60 0 3000 100 1 0 0 0 0 > ../output/$file.log &
+./system 60 0 3000 100 1 0 0 0 0 > ../output/$file.log
 
 file="120-0-3000-100-fixed"
-./system 120 0 3000 100 1 0 0 0 0 > ../output/$file.log &
+./system 120 0 3000 100 1 0 0 0 0 > ../output/$file.log
 
 file="5-0-3000-100-histogram"
-./system 120 0 3000 100 1 50 0.05 0.10 0.95 > ../output/$file.log &
+./system 120 0 3000 100 1 50 0.05 0.10 0.95 > ../output/$file.log
 
-file="5-0-2000-100-histogram"
-./system 120 0 2000 100 1 50 0.05 0.10 0.95 > ../output/$file.log &
+# file="5-0-2000-100-histogram"
+# ./system 120 0 2000 100 1 50 0.05 0.10 0.95 > ../output/$file.log
 
-file="5-0-1500-100-histogram"
-./system 120 0 1500 100 1 50 0.05 0.10 0.95 > ../output/$file.log &
+# file="5-0-1500-100-histogram"
+# ./system 120 0 1500 100 1 50 0.05 0.10 0.95 > ../output/$file.log
 
-file="5-0-1000-100-histogram"
-./system 120 0 1000 100 1 50 0.05 0.10 0.95 > ../output/$file.log &
+# file="5-0-1000-100-histogram"
+# ./system 120 0 1000 100 1 50 0.05 0.10 0.95 > ../output/$file.log
 
-file="5-0-800-100-histogram"
-./system 120 0 800 100 1 50 0.05 0.10 0.95 > ../output/$file.log &
+# file="5-0-800-100-histogram"
+# ./system 120 0 800 100 1 50 0.05 0.10 0.95 > ../output/$file.log
 
 # 确认当上面所有程序结束后, 再执行以下命令, 该怎么确认？
-wait
+wait ${pids[@]}
 
 # 所有后台进程完成后执行的命令
 cd ../.. && ./draw.sh

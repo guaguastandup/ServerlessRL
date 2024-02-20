b61c67ad9eb96f41798e907afb579340f9f1cc31
guaguastandup <45114416+guaguastandup@users.noreply.github.com>
2024-02-20 15:48:17 +0800

    update script

--- start.sh
+++ start.sh
@@ -19,60 +19,38 @@ trap cleanup SIGINT
 
 cd pkg/system && go build
 
+for keepAlive in 5 10 15 30 60 120
+do
+    for policy in 'random' 'maxmem' 'maxKeepAlive' 'minUsage' 'maxColdStartRate' 'lru'
+    do 
+        fixed=1
+        memory=3000
+        arrivalCnt=50
+        prewarm=0
+        policy='lru'
+        file="fixed-$policy-$i-$prewarm-$memory-$arrivalCnt"
+        ./system $keepAlive $prewarm $memory $arrivalCnt $fixed 0 0 0 0 $policy > ../output/$file.log &
+    done
+done
 
+for keepAlive in 5 10 15 30 60 120
+do
+    for policy in 'random' 'maxmem' 'maxKeepAlive' 'minUsage' 'maxColdStartRate' 'lru'
+    do 
+        fixed=0
+        memory=3000
+        arrivalCnt=50
+        prewarm=0
+        policy='lru'
+        file="fixed-$policy-$i-$prewarm-$memory-$arrivalCnt"
+        sum=50
+        leftBound=0.05
+        leftBound2=0.10
+        rightBound=0.95
+        ./system $keepAlive $prewarm $memory $arrivalCnt $fixed $sum $leftBound $leftBound2 $rightBound $policy > ../output/$file.log &
+    done
+done
 
-# ----------------------  fixed ------------------------------
-# interate
-# file="fixed-lru-5-0-3000-50"
-# ./system 5 0 3000 50 1 0 0 0 0 > ../output/$file.log &
-
-# file="fixed-lru-10-0-3000-50"
-# ./system 10 0 3000 50 1 0 0 0 0 > ../output/$file.log &
-
-# ------------------  fixed & policy ------------------------------
-# file="fixed-lru-5-0-3000-50"
-# ./system 5 0 3000 50 1 0 0 0 0 > ../output/$file.log &
-
-# file="fixed-lru-10-0-3000-50"
-# ./system 10 0 3000 50 1 0 0 0 0 > ../output/$file.log &
-
-# ---------------------- histogram -----------------------------
-# file="histogram-none-5-0-3000"
-# ./system 5 0 3000 50 1 50 0.05 0.10 0.95 lru > ../output/$file.log &
-
-# ------------------ histogram & policy-----------------------------
-file="5-0-600-50-maxmem"
-./system 5 0 600 50 1 50 0.05 0.10 0.95 maxmem > ../output/$file.log &
-file="5-0-800-50-maxmem"
-./system 5 0 800 50 1 50 0.05 0.10 0.95 maxmem > ../output/$file.log &
-file="5-0-1000-50-maxmem"
-./system 5 0 1000 50 1 50 0.05 0.10 0.95 maxmem > ../output/$file.log &
-
-# file="5-0-1500-50-random"
-# ./system 5 0 1500 50 1 50 0.05 0.10 0.95 random > ../output/$file.log &
-
-# file="5-0-1500-50-maxKeepAlive"
-# ./system 5 0 1500 50 1 50 0.05 0.10 0.95 maxKeepAlive > ../output/$file.log &
-
-# file="5-0-1500-50-minUsage"
-# ./system 5 0 1500 50 1 50 0.05 0.10 0.95 minUsage > ../output/$file.log &
-
-# file="5-0-1500-50-maxColdStartRate"
-# ./system 5 0 1500 50 1 50 0.05 0.10 0.95 maxColdStartRate > ../output/$file.log &
-
-# file="5-0-1500-50-fixed-lru" 
-# ./system 5 0 1500 50 1 50 0 0 0 lru > ../output/$file.log &
-
-# file="5-0-1500-50-fixed-random" 
-# ./system 5 0 1500 50 1 50 0 0 0 random > ../output/$file.log &
-
-# file="30-0-1500-50-fixed-random" 
-# ./system 30 0 1500 50 1 50 0 0 0 lru > ../output/$file.log &
-
-# file="30-0-1500-50-fixed-maxmem" 
-# ./system 30 0 1500 50 1 50 0 0 0 maxmem > ../output/$file.log &
-
-# 确认当上面所有程序结束后, 再执行以下命令, 该怎么确认？
 wait ${pids[@]}
 
 # 所有后台进程完成后执行的命令
